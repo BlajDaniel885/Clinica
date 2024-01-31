9e63573c49c5766f259aad1f412eb775ecc79465
Vlad Bulete <vladone1996@gmail.com>
2024-01-31 20:48:53 +0200

    fix missing employee controller

---
+++ Clinica/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinica.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
