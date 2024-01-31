612789b2e64ddb8ea8e861f192f43c52d02c8f3a
Vlad Bulete <vladone1996@gmail.com>
2024-01-31 20:58:22 +0200

    add tasks

--- Clinica/Controllers/HomeController.cs
+++ Clinica/Controllers/HomeController.cs
@@ -43,6 +43,8 @@ namespace Clinica.Controllers
         [HttpPost]
         public async Task<IActionResult> Authenticate(string txtUserName, string txtPassword)
         {
+            // TODO: if (UserAndPasswordMatches(username, password) != null)
+
             if((txtUserName.ToLower()=="admin") && (txtPassword == "123"))
             {
                 var claims = new List<Claim>
@@ -67,5 +69,17 @@ namespace Clinica.Controllers
             await HttpContext.SignOutAsync();
             return RedirectToAction("Index");
         }
+
+        // TODO: declarare UserAndPasswordMatches()
+        // returneaza userul impreuna cu rolurile sale
+        // TODO:trebuie facut prima data apel catre tabelul de user si verificat username si parola 
+        // TODO:apoi trebuie facut apel catre tabela inter-user-rol si tabela rol ca sa aduci rolurile utilizatorului ca string aka "admin"/ "doctor"/ "pacient" (nu id : 1,2,3)
+        // TODO:hint: iti trebuie un ViewModel (adica o clasa noua) care sa contina tot ce contine clasa de user si in plus va avea si o lista de string-uri pt roluri.    
+        // TODO:clasa de LoginViewModel sau UserViewModel sau LoginVM prescurtat, trebuie pusa intr-un Folder separat numit ViewModels, la fel ca Views, Controllers, Properties.
+
+        // TODO:Muta tot ce tine de login, auth in controllerul de Account
+
+        // TODO:Authorizare pe baza de cookies
+        // Cookie Authorization - pe google
     }
 }
